c182aeb0038f37c1c63da7a332776f832a3037ed
Cally Makhubele <makhubelecally902@gmail.com>
2024-07-31 22:51:04 +0200

    Improved code

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.getElementById('menu-icon');
+    const navbar = document.querySelector('.navbar');
+
+    menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+    });
+
+   
+});
